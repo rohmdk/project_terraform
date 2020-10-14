68e0a10a181bba55400ecf05489c3ad0fe60c09a
rohmdk <rohmdk@gmail.com>
2020-10-15 01:35:07 +0200

    Update azure-example.tf

--- azure-example.tf
+++ azure-example.tf
@@ -168,19 +168,19 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
 ###
 
 resource "azurerm_resource_group" "example" {
-  name     = "tfex-recovery_vault"
+  name     = "VM-recovery_vault"
   location = "eastus"
 }
 
 resource "azurerm_recovery_services_vault" "example" {
-  name                = "tfex-recovery-vault"
+  name                = "VM-recovery_vault"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   sku                 = "Standard"
 }
 
 resource "azurerm_backup_policy_vm" "example" {
-  name                = "tfex-recovery-vault-policy"
+  name                = "VM-recovery_vault-policy"
   resource_group_name = azurerm_resource_group.example.name
   recovery_vault_name = azurerm_recovery_services_vault.example.name
 
